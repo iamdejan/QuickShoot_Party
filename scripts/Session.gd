7d4627781bb2df50cfad865036ace1c74310770c
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-05 15:09:19 +0700

    Implementing Session to support Register, Login, and Logout

--- scripts/Session.gd
+++ scripts/Session.gd
@@ -1,7 +1,59 @@
 extends Node
 
-const prefix = "res://scenes/"
-const extension = "tscn"
+const prefix: String = "res://scenes/"
+const extension: String = "tscn"
 
-static func build_scene_URL(key) -> String:
+func build_scene_URL(key) -> String:
 	return prefix + key + "." + extension
+
+const user_data = {
+	"name": "",
+	"id": -1
+}
+
+var scene_source: String = ""
+
+func login(user_id: String) -> void:
+	scene_source = "Login"
+	var headers = ["Content-Type: application/json"]
+	$HTTPRequest.request("http://quickshoot-party-data-server.herokuapp.com/users/" + user_id, headers)
+	pass
+
+func register(name: String) -> void:
+	scene_source = "Register"
+	
+	var data = {
+		"name" : name
+	}
+	
+	var json_sent = JSON.print(data)
+	var headers = ["Content-Type: application/json"]
+	
+	$HTTPRequest.request("http://quickshoot-party-data-server.herokuapp.com/users/create", headers, true, HTTPClient.METHOD_POST, json_sent)
+	pass
+
+func set_user_data(user: Dictionary) -> void:
+	user_data["name"] = user["name"]
+	user_data["id"] = int(user["id"])
+	pass
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json
+	var data: Dictionary
+	var user: Dictionary
+	if result == $HTTPRequest.RESULT_SUCCESS and body.size() > 0:
+		json = JSON.parse(body.get_string_from_utf8())
+		data = json.result
+		if data["success"] == true:
+			user = data["result"]
+			set_user_data(user)
+			get_tree().change_scene(Session.build_scene_URL("Dashboard"))
+		else:
+			get_tree().get_root().get_node("/root/" + scene_source + "/SubmitButton").disabled = false
+			get_tree().get_root().get_node("/root/" + scene_source + "/ErrorLabel").text = data.message
+	pass
+
+func flush() -> void:
+	user_data["name"] = ""
+	user_data["id"] = -1
+	pass
