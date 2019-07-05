b137e34b8e19a5bd9169e69e44bb074c6952cc5f
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-05 15:08:55 +0700

    Implement Logout feature

--- scripts/Dashboard.gd
+++ scripts/Dashboard.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _ready():
+	$UserIDLabel.text += " " + String(Session.user_data["id"])
+	$UserNameLabel.text += " " + String(Session.user_data["name"])
+	pass
+
 func _on_CreateRoomButton_pressed():
 	#API Call will be implemented later
 	
@@ -10,4 +15,10 @@ func _on_JoinRoomButton_pressed():
 	#API Call will be implemented later
 	
 	get_tree().change_scene(Session.build_scene_URL("JoinRoom"))
+	pass
+
+
+func _on_LogoutButton_pressed():
+	Session.flush()
+	get_tree().change_scene(Session.build_scene_URL("Home"))
 	pass
